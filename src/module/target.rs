3220ad619962acb68f58d07e6e8f289132b11a70
wukong <>
2025-05-28 15:29:08 +0800

    update mod spec

--- src/module/target.rs
+++ src/module/target.rs
@@ -3,7 +3,6 @@ use std::{
     str::FromStr,
 };
 
-use crate::vars::VarCollection;
 use async_trait::async_trait;
 use derive_getters::Getters;
 use orion_error::{ErrorOwe, ErrorWith, WithContext};
@@ -13,10 +12,13 @@ use crate::{
     addr::path_file_name,
     artifact::ArtifactPackage,
     conf::ConfSpec,
-    error::SpecResult,
+    const_vars::SPEC_DIR,
+    error::{SpecReason, SpecResult, ToErr},
     resource::CaculateResSpec,
     software::LogsSpec,
-    types::{AsyncUpdateable, Configable, Persistable},
+    tpl::{TPlEngineType, TplRender},
+    types::{AsyncUpdateable, Configable, JsonAble, Localizable, Persistable},
+    vars::{ValueDict, VarCollection},
 };
 
 use super::TargetNode;
@@ -30,6 +32,7 @@ pub struct ModTargetSpec {
     logs_spec: LogsSpec,
     res_spec: CaculateResSpec,
     vars: VarCollection,
+    local: Option<PathBuf>,
 }
 
 #[async_trait]
@@ -42,44 +45,50 @@ impl AsyncUpdateable for ModTargetSpec {
 
 impl Persistable<ModTargetSpec> for ModTargetSpec {
     fn save_to(&self, root: &Path) -> SpecResult<()> {
-        let target_path = root.join(self.target().to_string());
+        let target_path = root.join(self.target().to_string()).join(SPEC_DIR);
         std::fs::create_dir_all(&target_path)
             .owe_conf()
             .with(format!("path: {}", target_path.display()))?;
-        let artifact_path = target_path.join(crate::const_vars::ARTIFACT_YML);
+        let artifact_path = target_path.join("artifact.yml");
         self.artifact.save_conf(&artifact_path)?;
 
         self.actions.save_to(&target_path)?;
-        let spec_path = target_path.join(crate::const_vars::CONF_SPEC_YML);
+        let spec_path = target_path.join("conf_spec.yml");
         self.conf_spec.save_conf(&spec_path)?;
-        let spec_path = target_path.join(crate::const_vars::LOGS_SPEC_YML);
+        let spec_path = target_path.join("logs_spec.yml");
         self.logs_spec.save_conf(&spec_path)?;
 
-        let spec_path = target_path.join(crate::const_vars::RES_SPEC_YML);
+        let spec_path = target_path.join("res_spec.yml");
         self.res_spec.save_conf(&spec_path)?;
-        let vars_path = target_path.join(crate::const_vars::VARS_YML);
+        let vars_path = target_path.join("vars.yml");
         self.vars.save_conf(&vars_path)?;
         Ok(())
     }
 
-    fn load_from(target_path: &Path) -> SpecResult<Self> {
-        let mut ctx = WithContext::want("load mod spec");
-        let artifact_path = target_path.join(crate::const_vars::ARTIFACT_YML);
-        ctx.with("artifact", format!("{}", artifact_path.display()));
-        let artifact = ArtifactPackage::from_conf(&artifact_path).with(&ctx)?;
+    fn load_from(root_path: &Path) -> SpecResult<Self> {
+        let mut ctx = WithContext::want("load target mod spec");
 
-        let actions = Actions::load_from(target_path).with(&ctx)?;
-        let spec_path = target_path.join(crate::const_vars::CONF_SPEC_YML);
-        let conf_spec = ConfSpec::from_conf(&spec_path).with(&ctx)?;
-        let spec_path = target_path.join(crate::const_vars::LOGS_SPEC_YML);
-        let logs_spec = LogsSpec::from_conf(&spec_path).with(&ctx)?;
-        let spec_path = target_path.join(crate::const_vars::RES_SPEC_YML);
-        let res_spec = CaculateResSpec::from_conf(&spec_path).with(&ctx)?;
-        let vars_path = target_path.join(crate::const_vars::VARS_YML);
-        let vars = VarCollection::from_conf(&vars_path).with(&ctx)?;
-        let target = TargetNode::from_str(path_file_name(target_path)?.as_str())
+        let target = TargetNode::from_str(path_file_name(&root_path)?.as_str())
             .owe_res()
             .with(&ctx)?;
+        let target_path = root_path.join(SPEC_DIR);
+        let artifact_path = target_path.join("artifact.yml");
+        ctx.with_path("artifact", &artifact_path);
+        let artifact = ArtifactPackage::from_conf(&artifact_path).with(&ctx)?;
+
+        let actions = Actions::load_from(&target_path).with(&ctx)?;
+        let spec_path = target_path.join("conf_spec.yml");
+        ctx.with_path("conf_spec", &spec_path);
+        let conf_spec = ConfSpec::from_conf(&spec_path).with(&ctx)?;
+        let logs_path = target_path.join("logs_spec.yml");
+        ctx.with_path("logs_spec", &logs_path);
+        let logs_spec = LogsSpec::from_conf(&logs_path).with(&ctx)?;
+        let res_path = target_path.join("res_spec.yml");
+        ctx.with_path("res_spec", &logs_path);
+        let res_spec = CaculateResSpec::from_conf(&res_path).with(&ctx)?;
+        let vars_path = target_path.join("vars.yml");
+        ctx.with_path("vars", &vars_path);
+        let vars = VarCollection::from_conf(&vars_path).with(&ctx)?;
 
         Ok(Self {
             target,
@@ -88,6 +97,7 @@ impl Persistable<ModTargetSpec> for ModTargetSpec {
             conf_spec,
             logs_spec,
             res_spec,
+            local: Some(root_path.to_path_buf()),
             vars,
         })
     }
@@ -108,7 +118,30 @@ impl ModTargetSpec {
             conf_spec,
             logs_spec: LogsSpec::tpl_init(),
             res_spec,
+            local: None,
             vars,
         }
+    }
+}
+
+#[async_trait]
+impl Localizable for ModTargetSpec {
+    async fn localize(&self) -> SpecResult<()> {
+        let mut ctx = WithContext::want("modul localize");
+        let local = self
+            .local
+            .clone()
+            .ok_or(SpecReason::Miss("local-path".into()).to_err().with(&ctx))?;
+        let tpl = local.join(crate::const_vars::SPEC_DIR);
+        let dst = local.join(crate::const_vars::LOCAL_DIR);
+        let data = local.join(crate::const_vars::VALUE_JSON);
+        ctx.with_path("dst", &dst);
+        self.update_local(&tpl).await?;
+        if !data.exists() {
+            let vars_dict = self.vars.value_dict();
+            vars_dict.save_json(&data)?;
+        }
+        TplRender::render_path(TPlEngineType::Handlebars, &tpl, &dst, &data).with(&ctx)?;
+        Ok(())
     }
 }
