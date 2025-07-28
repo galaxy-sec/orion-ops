565849baacc5351e4519356e08fd41530c3180a0
wukong <sec-wukong@outlook.com>
2025-07-28 09:52:22 +0800

    update gxl init

--- tests/sys_test.rs
+++ tests/sys_test.rs
@@ -3,9 +3,10 @@ use std::path::{Path, PathBuf};
 use orion_error::{ErrorOwe, TestAssertWithMsg};
 use orion_infra::path::make_clean_path;
 use orion_ops::{
-    const_vars::SYS_MODEL_PRJ_ROOT,
+    const_vars::{SYS_MODEL_PRJ_ROOT, WORKINS_PRJ_ROOT},
     error::MainResult,
     module::depend::{Dependency, DependencySet},
+    ops_prj::proj::OpsProject,
     system::{proj::SysProject, spec::SysModelSpec},
     types::LocalizeOptions,
 };
@@ -17,15 +18,36 @@ use orion_variate::{
 };
 #[tokio::test]
 async fn test_all() -> MainResult<()> {
+    test_init();
     let sys_proj = make_sys_prj_example().await?;
     let out_path = PathBuf::from(SYS_MODEL_PRJ_ROOT).join("example_sys_x.tar.gz");
     compress(sys_proj.root_local(), &out_path).owe_sys()?;
+    let mut ops_proj = make_workins_example().await?;
+    ops_proj
+        .import_sys(
+            out_path.display().to_string().as_str(),
+            &UpdateOptions::for_test(),
+        )
+        .await?;
+
     Ok(())
     //sys_proj.
 }
-async fn make_sys_prj_example() -> MainResult<SysProject> {
+async fn make_workins_example() -> MainResult<OpsProject> {
     test_init();
+    let prj_path = PathBuf::from(WORKINS_PRJ_ROOT).join("workins_sys_x");
+    make_clean_path(&prj_path).owe_logic()?;
+    let project = OpsProject::for_test("workins_sys_x").assert("make workins");
+    project.save().assert("save workins_prj");
+    let project = OpsProject::load(&prj_path).assert("workins-prj");
+    let project = project
+        .update(&UpdateOptions::default())
+        .await
+        .assert("spec.update_local");
+    Ok(project)
+}
 
+async fn make_sys_prj_example() -> MainResult<SysProject> {
     let prj_path = PathBuf::from(SYS_MODEL_PRJ_ROOT).join("example_sys_x");
     make_clean_path(&prj_path).owe_logic()?;
     let project = make_sys_prj_testins(&prj_path).assert("make cust");
